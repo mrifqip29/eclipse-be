540db841ff49509d8e116f1a9dce1d9a77aa85c8
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-11-07 16:45:52 +0700

    Add controller template

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { welcomeController } from '../controllers';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to Eclipse Website API' });
-});
+router.get('/', welcomeController);
 
 export default router;
